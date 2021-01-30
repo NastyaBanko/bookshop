97bdec6a7698692aa2ec3003fd80c178f46e4305
NastyaBanko <nastyabanko@gmail.com>
2021-01-30 18:33:28 +0300

    no-changes

--- src/app/components/orderview-modal/orderview-modal.component.ts
+++ src/app/components/orderview-modal/orderview-modal.component.ts
@@ -50,7 +50,7 @@ export class OrderviewModalComponent extends RxUnsubscribe implements OnInit {
     this.httpService
       .changeOrderStatus(id, status)
       .subscribe(() => {
-        console.log('change statusssss');
+        console.log('change status123');
       });
   }
 
